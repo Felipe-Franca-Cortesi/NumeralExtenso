18a707c5a3519be23cc71b7ee9dbc78420533ef2
Felipe França <contatofelipefranca12@gmail.com>
2020-10-07 21:15:58 -0300

    Adiciona a conclusão do exercício NumeralExtenso

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Humanizer;
+
+namespace NumeralExtenso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("--- Numeral Extenso");
+            Console.Write("Digite qualquer número: ");
+            int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+            string numeroExtenso = numeroDigitado.ToWords();
+            Console.WriteLine($"Número por extenso: {numeroExtenso}");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # NumeralExtenso
-[console: NumeralExtenso] Receba um número inteiro e exiba-o por extenso.
+*[console: NumeralExtenso] Receba um número inteiro e exiba-o por extenso.*
+
+`Exemplo:`
+
+```
+--- Numeral Extenso
+Digite qualquer número: 125
+Número por extenso: cento e vinte e cinco
+```
+
